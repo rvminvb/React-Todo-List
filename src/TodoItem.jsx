94306a98e9701d0c3e7fbf209e0cc2b43e56778e
Ramina Burkhanova <rvminvb@yandex.ru>
2021-02-22 11:59:46 +0300

    update readme fix styles

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -2,20 +2,14 @@ import React from "react";
 import logo from "./icons/dump.svg";
 import flag from "./icons/flag.svg";
 import { Draggable } from 'react-beautiful-dnd';
+import './ToDoItem.css'
 
 function TodoItem({ index, item, onCheck, onMark }) {
-
-    const liStyle = {
-        display: 'flex',
-        justifyContent: 'space-between',
-        backgroundColor: item.priority === 1 ? '#FFCBDB' : "#f1f5f8"
-    };
-
-   return (
+    return (
         <>
         <Draggable draggableId={item.id} index={index}>
             {provided =>(
-        <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >  
+        <li className={"item" + (item.priority === 1 ? " priority" : "")} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >  
             {item.name}  
             <img src={flag} alt="flag" width='25px' onClick={() => {onMark(item.id)}}/>
             <img src={logo} alt="trash bin" width='25px' onClick={() => {onCheck(item.id)}}/>
